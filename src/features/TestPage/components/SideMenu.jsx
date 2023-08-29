a05798390c2a837ffaaa60285572a1d8b84f44a6
vijeesh v <vijeesh.v@gjglobalsoft.com>
2023-08-29 20:19:33 +0530

    latest

--- src/features/TestPage/components/SideMenu.jsx
+++ src/features/TestPage/components/SideMenu.jsx
@@ -15,144 +15,156 @@ import profilePic from "../../../assets/images/Sachin-Tendulkar_(cropped).jpg";
 const SideMenu = ({ menu, setMenu, selection, setSelection }) => {
   return (
     <>
-      <section className="side-menu">
-        <section className="side-menu-container">
-          <section className="side-menu-logo">
-            <p>LOGO</p>
-          </section>
-          <section className="side-menu-profile">
-            <section className="side-menu-photo">
-              <section>
-                <img className="side-menu-photo-box" src={profilePic} alt="/" />
+      {menu === false && (
+        <section className="side-menu">
+          <section className="side-menu-container">
+            <section className="side-menu-logo">
+              <p>LOGO</p>
+            </section>
+            <section className="side-menu-profile">
+              <section className="side-menu-photo">
+                <section>
+                  <img
+                    className="side-menu-photo-box"
+                    src={profilePic}
+                    alt="/"
+                  />
+                </section>
+              </section>
+              <section className="side-menu-profile-name">
+                <span>Ram Mohan</span>
+                <FontAwesomeIcon
+                  style={{
+                    fontSize: "12px",
+                    marginLeft: "2%",
+                    cursor: "pointer",
+                  }}
+                  icon={faChevronRight}
+                />
+              </section>
+              <section className="side-menu-profile-email">
+                <span>rammohan2@gmail.com</span>
               </section>
             </section>
-            <section className="side-menu-profile-name">
-              <span>Ram Mohan</span>
+            <section className="menu-items">
+              {selection === 0 ? (
+                <section className="menu-1">
+                  <section className="menu-item-1">
+                    <FontAwesomeIcon
+                      style={{ marginRight: "5%" }}
+                      icon={faLayerGroup}
+                    />
+                    Dashboard
+                  </section>
+                </section>
+              ) : (
+                <section className="menu-2" onClick={() => setSelection(0)}>
+                  <section className="menu-item-2">
+                    <FontAwesomeIcon
+                      style={{ marginRight: "5%" }}
+                      icon={faLayerGroup}
+                    />
+                    Dashboard
+                  </section>
+                </section>
+              )}
+              {selection !== 1 ? (
+                <section className="menu-2" onClick={() => setSelection(1)}>
+                  <section className="menu-item-2">
+                    <FontAwesomeIcon
+                      style={{ marginRight: "5%" }}
+                      icon={faSpa}
+                    />
+                    Perks
+                  </section>
+                </section>
+              ) : (
+                <section className="menu-1">
+                  <section className="menu-item-1">
+                    <FontAwesomeIcon
+                      style={{ marginRight: "5%" }}
+                      icon={faSpa}
+                    />
+                    Perks
+                  </section>
+                </section>
+              )}
+              {selection !== 2 ? (
+                <section className="menu-3" onClick={() => setSelection(2)}>
+                  <section className="menu-item-3">
+                    <FontAwesomeIcon
+                      style={{ marginRight: "5%" }}
+                      icon={faPuzzlePiece}
+                    />
+                    Addons
+                  </section>
+                </section>
+              ) : (
+                <section className="menu-1">
+                  <section className="menu-item-1">
+                    <FontAwesomeIcon
+                      style={{ marginRight: "5%" }}
+                      icon={faPuzzlePiece}
+                    />
+                    Addons
+                  </section>
+                </section>
+              )}
+              {selection !== 3 ? (
+                <section className="menu-4" onClick={() => setSelection(3)}>
+                  <section className="menu-item-4">
+                    <FontAwesomeIcon
+                      style={{ marginRight: "5%" }}
+                      icon={faComments}
+                    />
+                    FAQ
+                  </section>
+                </section>
+              ) : (
+                <section className="menu-1">
+                  <section className="menu-item-1">
+                    <FontAwesomeIcon
+                      style={{ marginRight: "5%" }}
+                      icon={faComments}
+                    />
+                    FAQ
+                  </section>
+                </section>
+              )}
+              {selection !== 4 ? (
+                <section className="menu-5" onClick={() => setSelection(4)}>
+                  <section className="menu-item-5">
+                    <FontAwesomeIcon
+                      style={{ marginRight: "5%" }}
+                      icon={faUser}
+                    />
+                    Support
+                  </section>
+                </section>
+              ) : (
+                <section className="menu-1">
+                  <section className="menu-item-1">
+                    <FontAwesomeIcon
+                      style={{ marginRight: "5%" }}
+                      icon={faUser}
+                    />
+                    Support
+                  </section>
+                </section>
+              )}
+            </section>
+          </section>
+          <section className="side-menu-bottom">
+            <section>
+              <span>Logout</span>
               <FontAwesomeIcon
-                style={{
-                  fontSize: "12px",
-                  marginLeft: "2%",
-                  cursor: "pointer",
-                }}
-                icon={faChevronRight}
+                style={{ fontSize: "13px", marginLeft: "10px" }}
+                icon={faPowerOff}
               />
             </section>
-            <section className="side-menu-profile-email">
-              <span>rammohan2@gmail.com</span>
-            </section>
-          </section>
-          <section className="menu-items">
-            {selection === 0 ? (
-              <section className="menu-1">
-                <section className="menu-item-1">
-                  <FontAwesomeIcon
-                    style={{ marginRight: "5%" }}
-                    icon={faLayerGroup}
-                  />
-                  Dashboard
-                </section>
-              </section>
-            ) : (
-              <section className="menu-2" onClick={() => setSelection(0)}>
-                <section className="menu-item-2">
-                  <FontAwesomeIcon
-                    style={{ marginRight: "5%" }}
-                    icon={faLayerGroup}
-                  />
-                  Dashboard
-                </section>
-              </section>
-            )}
-            {selection !== 1 ? (
-              <section className="menu-2" onClick={() => setSelection(1)}>
-                <section className="menu-item-2">
-                  <FontAwesomeIcon style={{ marginRight: "5%" }} icon={faSpa} />
-                  Perks
-                </section>
-              </section>
-            ) : (
-              <section className="menu-1">
-                <section className="menu-item-1">
-                  <FontAwesomeIcon style={{ marginRight: "5%" }} icon={faSpa} />
-                  Perks
-                </section>
-              </section>
-            )}
-            {selection !== 2 ? (
-              <section className="menu-3" onClick={() => setSelection(2)}>
-                <section className="menu-item-3">
-                  <FontAwesomeIcon
-                    style={{ marginRight: "5%" }}
-                    icon={faPuzzlePiece}
-                  />
-                  Addons
-                </section>
-              </section>
-            ) : (
-              <section className="menu-1">
-                <section className="menu-item-1">
-                  <FontAwesomeIcon
-                    style={{ marginRight: "5%" }}
-                    icon={faPuzzlePiece}
-                  />
-                  Addons
-                </section>
-              </section>
-            )}
-            {selection !== 3 ? (
-              <section className="menu-4" onClick={() => setSelection(3)}>
-                <section className="menu-item-4">
-                  <FontAwesomeIcon
-                    style={{ marginRight: "5%" }}
-                    icon={faComments}
-                  />
-                  FAQ
-                </section>
-              </section>
-            ) : (
-              <section className="menu-1">
-                <section className="menu-item-1">
-                  <FontAwesomeIcon
-                    style={{ marginRight: "5%" }}
-                    icon={faComments}
-                  />
-                  FAQ
-                </section>
-              </section>
-            )}
-            {selection !== 4 ? (
-              <section className="menu-5" onClick={() => setSelection(4)}>
-                <section className="menu-item-5">
-                  <FontAwesomeIcon
-                    style={{ marginRight: "5%" }}
-                    icon={faUser}
-                  />
-                  Support
-                </section>
-              </section>
-            ) : (
-              <section className="menu-1">
-                <section className="menu-item-1">
-                  <FontAwesomeIcon
-                    style={{ marginRight: "5%" }}
-                    icon={faUser}
-                  />
-                  Support
-                </section>
-              </section>
-            )}
           </section>
         </section>
-        <section className="side-menu-bottom">
-          <section>
-            <span>Logout</span>
-            <FontAwesomeIcon
-              style={{ fontSize: "13px", marginLeft: "10px" }}
-              icon={faPowerOff}
-            />
-          </section>
-        </section>
-      </section>
+      )}
       {menu === false ? (
         <section className="side-menu-small-window">
           <section className="side-menu-small-window-container">
